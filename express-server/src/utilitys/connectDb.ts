099200cfb0ccaa7c85978f615904f0df63c908b9
technologicaljerry <cserajatverma@gmail.com>
2023-10-12 22:03:41 +0530

    Update connectDb.ts

--- express-server/src/utilitys/connectDb.ts
+++ express-server/src/utilitys/connectDb.ts
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 import config from 'config';
+import logger from "./logger";
 
 function connect() {
     const dbUrl = config.get<string>('databadeUrl');
+
+    try {
+        mongoose.connect(dbUrl);
+        logger.info("DB connected");
+    } catch (error) {
+        logger.error("Could not connect to db");
+        process.exit(1);
+    }
 }
+
+export default connect;
