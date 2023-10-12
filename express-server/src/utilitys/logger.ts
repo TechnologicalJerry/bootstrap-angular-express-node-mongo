bd2f318a2249088ceb667870c83501618eed7a17
technologicaljerry <cserajatverma@gmail.com>
2023-10-12 22:17:51 +0530

    Update logger.ts

--- express-server/src/utilitys/logger.ts
+++ express-server/src/utilitys/logger.ts
@@ -1,12 +1,27 @@
 import logger from "pino";
 import dayjs from "dayjs";
+import pino from "pino";
+import pinoPretty from "pino-pretty";
 
-const log = logger({
-    prettyPrint: true,
+// const log = logger({
+//     prettyPrint: true,
+//     base: {
+//         pid: false,
+//     },
+//     timestamp: () => `,"time":"${dayjs().format()}"`,
+// });
+
+// export default log;
+
+
+
+const log = pino({
     base: {
         pid: false,
     },
     timestamp: () => `,"time":"${dayjs().format()}"`,
+    // Use pino-pretty as a transport
+    prettifier: pinoPretty,
 });
 
 export default log;
