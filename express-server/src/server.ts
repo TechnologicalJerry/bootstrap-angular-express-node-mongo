13688ad38530847b93615ced49dc16a46925046b
technologicaljerry <cserajatverma@gmail.com>
2023-10-13 23:39:30 +0530

    Update server.ts

--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from 'express';
 import config from 'config';
 import connect from './utilitys/connectDb';
 
+
 const app = express();
 
 const PORT = config.get<number>('port');
+
+app.use(express.json());
 
 app.listen(PORT, async () => {
     console.log(`Server is running!!!!!!!! on PORT = ${PORT}`);
