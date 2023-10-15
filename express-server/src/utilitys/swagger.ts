32995807eecb4566f13d768bae3fe60091a29642
technologicaljerry <cserajatverma@gmail.com>
2023-10-15 20:22:48 +0530

    Update swagger.ts

--- express-server/src/utilitys/swagger.ts
+++ express-server/src/utilitys/swagger.ts
@@ -2,3 +2,28 @@ import { Express, Request, Response } from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import log from "./logger";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+        title: "REST API Docs",
+        version: "1.0.0"
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes.ts", "./src/schema/*.ts"],
+};
