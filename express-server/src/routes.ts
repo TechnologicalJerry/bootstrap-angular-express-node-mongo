c966e88a7bfe1b30d51879ba9ae6a1fa0d133555
technologicaljerry <cserajatverma@gmail.com>
2023-10-15 20:27:26 +0530

    Update routes.ts

--- express-server/src/routes.ts
+++ express-server/src/routes.ts
@@ -1,3 +1,7 @@
 import { Express, Request, Response } from "express";
 
-function routes(app: Express) { }
+function routes(app: Express) {
+
+    app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+}
