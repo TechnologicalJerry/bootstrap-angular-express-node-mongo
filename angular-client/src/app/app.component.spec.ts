b948ff43f5f7342dcf0841a77fbe4a9fe5c6b346
technologicaljerry <cserajatverma@gmail.com>
2024-01-09 21:14:31 +0530

    Update app.component.spec.ts

--- angular-client/src/app/app.component.spec.ts
+++ angular-client/src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-express-node-mongo');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-express-node-mongo'
+    );
   });
 });
