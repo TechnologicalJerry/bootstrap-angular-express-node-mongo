f2fcafd9bcea61e9636453c1a65e7d1ca04807f3
Rajat Verma <cserajatverma@gmail.com>
2024-01-10 22:15:58 +0530

    Update app.routes.ts

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  {path: 'about', component: AboutComponent, pathMatch: 'full'}
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
 ];
