6883db8909aa3be0ffbb32ed722c4517951a2f89
technologicaljerry <cserajatverma@gmail.com>
2024-01-27 22:56:08 +0530

    Create about.module.ts

---
+++ bootstrap-angular-client/src/app/about/about.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AboutRoutingModule,
+    RouterModule
+  ]
+})
+export class AboutModule { }
