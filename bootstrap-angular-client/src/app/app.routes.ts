9030dc6a37992da99103b516ca6bd6ee9a74904c
Rajat Verma <cserajatverma@gmail.com>
2024-02-18 18:15:37 +0530

    Delete app.routes.ts

--- bootstrap-angular-client/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-
-export const routes: Routes = [
-  { path: 'about', component: AboutComponent, pathMatch: 'full' },
-];
