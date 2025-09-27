0b03ca04fd1988a1d52c1fbd3d94c6a53eba9fa0
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-27 12:07:03 +0530

    Remove Swagger documentation setup
    Swagger-related dependencies, imports, and the swagger utility file were removed from the project. This simplifies the codebase and eliminates API documentation via Swagger.

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import config from 'config';
 import routes from "./routes";
-import swaggerDocs from "./utilitys/swagger";
 
 const app = express();
 
@@ -10,9 +8,5 @@ app.use(express.json());
 
 // Routes
 routes(app);
-
-// Swagger documentation
-const PORT = config.get<number>('port');
-swaggerDocs(app, PORT);
 
 export default app;
